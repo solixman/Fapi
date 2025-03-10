c05faec3e3d13a0c15acac122b6c33ab80d98a54
soulayman <soulaymanjaa09@gmail.com>
2025-03-10 16:02:17 +0100

    migrations

---
+++ Fapi/database/migrations/2025_03_10_144136_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('sender_email');
+            $table->string('reciever_email');
+            $table->string('status');
+            $table->date('date');
+            $table->integer('amount');
+            $table->string('motif');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
